d95b4f7f8ee15ae3eab3a50fba63fddb9d15798d
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2024-04-30 17:24:37 -0500

    servidor

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,9 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
-
+	
+	public static final int PUERTO =3400;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ServerSocket ss =null;
+		boolean continuar =true;
+		
+		System.out.println("Main Server ..");
+		
+		try {
+			ss = new ServerSocket(PUERTO);
+		}catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		while (continuar) {
+			Socket socket =ss.accept();
+			
+			try {
+				PrintWriter escritor = new PrintWriter(
+						socket.getOutputStream(),true);
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(socket.getInputStream()));
+				ProtocoloServidor.procesar(lector,escritor);
+				escritor.close();
+				lector.close();
+				socket.close();
+			}catch (IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
