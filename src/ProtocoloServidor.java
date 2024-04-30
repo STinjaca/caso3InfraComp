748e7d5cb8405e3dddb02ba0fa53a5efa7b90c8f
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2024-04-30 18:18:50 -0500

    servidor cambios

--- src/ProtocoloServidor.java
+++ src/ProtocoloServidor.java
@@ -1,19 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
 
 public class ProtocoloServidor {
-public static void procesar(BufferedReader pIn, PrintWriter pOut) throws IOException {
+public static void procesar(BufferedReader pIn, PrintWriter pOut, PrivateKey kPrivada) throws IOException {
 	String inputLine;
 	String outputLine;
 	
 	inputLine = pIn.readLine(); 
 	System.out.println("Entrada a procesar:"+inputLine);
+	String[] mensajes = inputLine.split(",");
 	
-	outputLine = inputLine;
+	outputLine = new String(CifradoAsimetrico.cifrar(kPrivada, "RSA" ,mensajes[1]),StandardCharsets.UTF_8);
 	
 	pOut.println(outputLine);
 	System.out.println("Salida procesada: "+outputLine);
 	
+	inputLine = pIn.readLine();
+	
+	if (inputLine.equals("ERROR")) {
+		return;
+	}
+	//else ok
+	System.out.println(inputLine);
+	
+	}
 }
-}
+
--- src/Servidor.java
+++ src/Servidor.java
@@ -4,13 +4,32 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class Servidor {
 	
 	public static final int PUERTO =3400;
 	
-
-	public static void main(String[] args) throws IOException {
+	public static PublicKey KPublica;
+	private static PrivateKey KPrivada;
+	
+	public Servidor() throws NoSuchAlgorithmException {
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(256);
+		KeyPair keyPair = generator.generateKeyPair();
+		
+		KPublica = keyPair.getPublic();
+		KPrivada = keyPair.getPrivate();
+	}
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		Servidor s = new Servidor();
+		s.ejecutar();
+	}
+	public void ejecutar() throws IOException, NoSuchAlgorithmException {
 		// TODO Auto-generated method stub
 		ServerSocket ss =null;
 		boolean continuar =true;
@@ -31,7 +50,7 @@ public class Servidor {
 						socket.getOutputStream(),true);
 				BufferedReader lector = new BufferedReader(
 						new InputStreamReader(socket.getInputStream()));
-				ProtocoloServidor.procesar(lector,escritor);
+				ProtocoloServidor.procesar(lector,escritor,this.KPrivada);
 				escritor.close();
 				lector.close();
 				socket.close();
