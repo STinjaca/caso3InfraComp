d0fe43d16b9ebca403343b6e9e7665b4fa61d953
Santi T <s.tinjaca@uniandes.edu.co>
2024-04-30 17:43:07 -0500

    Update ProtocoloCliente.java

--- src/ProtocoloCliente.java
+++ src/ProtocoloCliente.java
@@ -4,24 +4,25 @@ import java.io.PrintWriter;
 
 public class ProtocoloCliente {
 	public static void procesar(BufferedReader stdIn, BufferedReader pIn, PrintWriter pOut) throws IOException {
-		// lee del teclado
-		System.out.println("Escriba el mensaje para enviar: ");
-		String fromUser = stdIn.readLine();
-		
-		// envia por la red
-		pOut.println(fromUser);
-		
-		String fromServer = "";
-		
-		// lee lo que llega por la red 
-		// si lo que llega al servidor no es null
-		// observe la asignación luego la condición
-		
-		if((fromServer = pIn.readLine())!= null) {
-			System.out.println("Respuesta del servidor: " + fromServer);
+		boolean ejecutar = true;
+		while (ejecutar) {
 			
+			// lee del teclado
+			System.out.println("Escriba el mensaje para enviar: ");
+			String fromUser = stdIn.readLine();
+			
+			// envia por la red
+			pOut.println(fromUser);
+			
+			String fromServer = "";
+			
+			// lee lo que llega por la red 
+			// si lo que llega al servidor no es null
+			// observe la asignación luego la condición
+			
+			if((fromServer = pIn.readLine())!= null) {
+				System.out.println("Respuesta del servidor: " + fromServer);
+			}
 		}
-		
-		
 	}
 }
