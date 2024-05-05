5bfc390f34d03c345bb06ec9a42b69e9da523ea1
Santi T <s.tinjaca@uniandes.edu.co>
2024-05-05 11:44:35 -0500

    formato

--- src/ProtocoloCliente.java
+++ src/ProtocoloCliente.java
@@ -12,62 +12,63 @@ import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
 
 public class ProtocoloCliente {
-	public static void procesar(BufferedReader stdIn, BufferedReader pIn, PrintWriter pOut) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+	public static void procesar(BufferedReader stdIn, BufferedReader pIn, PrintWriter pOut)
+			throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
 		// lee del teclado
-		
+
 		String inputLine;
 		String outputLine;
-		
+
 		// SIMULACIÖN ESTABLECER LLAVES INICIALES
-		
+
 		// 1
 		System.out.println("iniciar");
 		pOut.println("Establcer llave");
-		
+
 		// 5
 		inputLine = pIn.readLine();
 		System.out.println(inputLine);
-		
+
 		System.out.println("Llegada llave");
 		// modulus
 		BigInteger modulus = new BigInteger(pIn.readLine());
 		BigInteger publicExponent = new BigInteger(pIn.readLine());
-		
+
 		// Crear LLave
 		RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(modulus, publicExponent);
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 		PublicKey kPublica = keyFactory.generatePublic(rsaPublicKeySpec);
 		System.out.println(kPublica.toString());
-		
+
 		// INICIO DE COMUNICACIÖN
 		// 1
 		System.out.println("Escriba el Reto a enviar: ");
 		String fromUser = stdIn.readLine();
-		
+
 		// enviar Reto
-		pOut.println("SECURE INIT,"+fromUser);
-		
+		pOut.println("SECURE INIT," + fromUser);
+
 		// 4
 		byte[] fromServerCifrado = Base64.getDecoder().decode(pIn.readLine());
 		byte[] fromServerDescifrado = CifradoAsimetrico.descifrar(kPublica, "RSA", fromServerCifrado);
 		String descifradoClaro = new String(fromServerDescifrado, StandardCharsets.UTF_8);
-		
+
 		// 5
-		if(!fromUser.equals(descifradoClaro)) {
+		if (!fromUser.equals(descifradoClaro)) {
 			pOut.println("ERROR");
 			return;
 		}
-		
+
 		pOut.println("OK");
 		System.out.println("OK");
-		
+
 		// 8
-		
+
 		BigInteger p = new BigInteger(pIn.readLine());
 		BigInteger g = new BigInteger(pIn.readLine());
 		BigInteger g_x = new BigInteger(pIn.readLine());
 		BigInteger iv = new BigInteger(pIn.readLine());
-		
+
 		descifradoClaro = "";
 		if (pIn.readLine().equals("SOBRE")) {
 			inputLine = pIn.readLine();
@@ -77,33 +78,34 @@ public class ProtocoloCliente {
 				descifradoClaro += new String(fromServerDescifrado, StandardCharsets.UTF_8);
 				inputLine = pIn.readLine();
 			}
-			
+
 		}
 
 		String[] descifradoClaroStrings = descifradoClaro.split(",");
-		
-		if(!descifradoClaroStrings[0].equals(p.toString())) {
+
+		if (!descifradoClaroStrings[0].equals(p.toString())) {
 			pOut.println("ERROR");
 			return;
 		}
-		if(!descifradoClaroStrings[1].equals(g.toString())) {
+		if (!descifradoClaroStrings[1].equals(g.toString())) {
 			pOut.println("ERROR");
 			return;
 		}
-		if(!descifradoClaroStrings[2].equals(g_x.toString())) {
+		if (!descifradoClaroStrings[2].equals(g_x.toString())) {
 			pOut.println("ERROR");
 			return;
 		}
 		// 9
 		pOut.println("OK");
 		System.out.println("9,OK");
-		
+
 		// 10
 		BigInteger y = new BigInteger("11");
 		BigInteger g_y = g.modPow(y, p);
 		pOut.println(g_y);
 		// 11b
-		BigInteger K = g_x.modPow(y, p);
+		BigInteger Kmaestra = g_x.modPow(y, p);
+		System.out.println(Kmaestra.bitCount());
 
 	}
 }
--- src/ProtocoloServidor.java
+++ src/ProtocoloServidor.java
@@ -9,93 +9,94 @@ import java.util.Base64;
 import java.util.Random;
 
 public class ProtocoloServidor {
-public static void procesar(BufferedReader pIn, PrintWriter pOut, PrivateKey kPrivada, PublicKey kPublica) throws IOException {
-	String inputLine;
-	String outputLine;	
-	
-	
-	// SIMULACIÖN ESTABLECER LLAVES INICIALES
-	//2
-	inputLine = pIn.readLine(); 
-	System.out.println("LLegada del cliente: " + inputLine);
-	
-	// 3
-	System.out.println("PUBLIC_KEY");
-	pOut.println("PUBLIC_KEY");
-	
-	System.out.println(kPublica.toString());
-	String[] kPublicaString = kPublica.toString().strip().split("\n");
-	String modulus = kPublicaString[2].split(":")[1].strip();
-	String publicExponent = kPublicaString[3].split(":")[1].strip();
-	
-	// 4
-	pOut.println(modulus);
-	pOut.println(publicExponent);
-	
-	// INICIO DE COMUNICACIÖN
-	// 2 Recibir reto
-	inputLine = pIn.readLine(); 
-	System.out.println("Entrada a procesar:"+inputLine);
-	String[] mensajes = inputLine.split(",");
-	// 3 Cifrar reto
-	byte[] mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA" ,mensajes[1]);
-	outputLine = Base64.getEncoder().encodeToString(mensajesCifrado);
-	pOut.println(outputLine);
-	System.out.println("Salida procesada: "+outputLine);
-	
-	inputLine = pIn.readLine();
-	
-	if (inputLine.equals("ERROR")) {
-		return;
-	}
-	//else ok
-	System.out.println(inputLine);
-	
-	// 6
-	BigInteger p = BigInteger.probablePrime(1024, new Random());
-	BigInteger  g =  new BigInteger("5");
-	BigInteger x = new BigInteger("10");
-	BigInteger g_x = g.modPow(x, p);
-	BigInteger iv = new BigInteger("100");
-	
-	// 7
-	pOut.println(p);
-	pOut.println(g);
-	pOut.println(g_x);
-	pOut.println(iv);
-	
-	outputLine = p.toString() + "," + g.toString() + "," + g_x.toString();
-	String outlineCifrado = "";
-	pOut.println("SOBRE");
-	if (outputLine.length() > 117) {
-	    int i = 0;
-	    while (i < outputLine.length()) {
-	        String tempString;
-	        int endIndex = Math.min(i + 117, outputLine.length());
-	        tempString = outputLine.substring(i, endIndex);
-	        mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA", tempString);
-	        outlineCifrado = Base64.getEncoder().encodeToString(mensajesCifrado);
-	        pOut.println(outlineCifrado);
-	        i = endIndex;
-	    }
-	} else {
-	    mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA", outputLine);
-	    outlineCifrado = Base64.getEncoder().encodeToString(mensajesCifrado);
-	    pOut.println(outlineCifrado);
-	}
-	
-	pOut.println("FIN SOBRE");
-	inputLine = pIn.readLine();
-	
-	if (inputLine.equals("ERROR")) {
-		return;
-	}
-	System.out.println(inputLine);
-	
-	// 11a
-	BigInteger g_y = new BigInteger(pIn.readLine());
-	BigInteger K = g_y.modPow(x, p);
+	public static void procesar(BufferedReader pIn, PrintWriter pOut, PrivateKey kPrivada, PublicKey kPublica)
+			throws IOException {
+		String inputLine;
+		String outputLine;
+
+		// SIMULACIÖN ESTABLECER LLAVES INICIALES
+		// 2
+		inputLine = pIn.readLine();
+		System.out.println("LLegada del cliente: " + inputLine);
+
+		// 3
+		System.out.println("PUBLIC_KEY");
+		pOut.println("PUBLIC_KEY");
+
+		System.out.println(kPublica.toString());
+		String[] kPublicaString = kPublica.toString().strip().split("\n");
+		String modulus = kPublicaString[2].split(":")[1].strip();
+		String publicExponent = kPublicaString[3].split(":")[1].strip();
+
+		// 4
+		pOut.println(modulus);
+		pOut.println(publicExponent);
+
+		// INICIO DE COMUNICACIÖN
+		// 2 Recibir reto
+		inputLine = pIn.readLine();
+		System.out.println("Entrada a procesar:" + inputLine);
+		String[] mensajes = inputLine.split(",");
+		// 3 Cifrar reto
+		byte[] mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA", mensajes[1]);
+		outputLine = Base64.getEncoder().encodeToString(mensajesCifrado);
+		pOut.println(outputLine);
+		System.out.println("Salida procesada: " + outputLine);
+
+		inputLine = pIn.readLine();
+
+		if (inputLine.equals("ERROR")) {
+			return;
+		}
+		// else ok
+		System.out.println(inputLine);
+
+		// 6
+		BigInteger p = BigInteger.probablePrime(1024, new Random());
+		BigInteger g = new BigInteger("5");
+		BigInteger x = new BigInteger("10");
+		BigInteger g_x = g.modPow(x, p);
+		BigInteger iv = new BigInteger("100");
+
+		// 7
+		pOut.println(p);
+		pOut.println(g);
+		pOut.println(g_x);
+		pOut.println(iv);
+
+		outputLine = p.toString() + "," + g.toString() + "," + g_x.toString();
+		String outlineCifrado = "";
+		pOut.println("SOBRE");
+		if (outputLine.length() > 117) {
+			int i = 0;
+			while (i < outputLine.length()) {
+				String tempString;
+				int endIndex = Math.min(i + 117, outputLine.length());
+				tempString = outputLine.substring(i, endIndex);
+				mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA", tempString);
+				outlineCifrado = Base64.getEncoder().encodeToString(mensajesCifrado);
+				pOut.println(outlineCifrado);
+				i = endIndex;
+			}
+		} else {
+			mensajesCifrado = CifradoAsimetrico.cifrar(kPrivada, "RSA", outputLine);
+			outlineCifrado = Base64.getEncoder().encodeToString(mensajesCifrado);
+			pOut.println(outlineCifrado);
+		}
+
+		pOut.println("FIN SOBRE");
+		inputLine = pIn.readLine();
+
+		if (inputLine.equals("ERROR")) {
+			return;
+		}
+		System.out.println(inputLine);
+
+		// 11a
+		BigInteger g_y = new BigInteger(pIn.readLine());
+		BigInteger Kmaestra = g_y.modPow(x, p);
+		System.out.println(Kmaestra.bitCount());
+
 
 	}
 }
-
